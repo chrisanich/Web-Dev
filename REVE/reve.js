3550aa2d684b9c7f646a3c24935179cfed6857b3
chrisanich <usefulhobbies@gmail.com>
2024-03-28 19:59:17 +0000

    New Icons Boxicons 1.0

--- REVE/reve.js
+++ REVE/reve.js
@@ -1,3 +1,4 @@
+/*Contact form*/
 function email() {
     let fname = document.getElementById("firstnamex").value;
     let lname = document.getElementById("lastnamex").value;
@@ -16,3 +17,94 @@ document.querySelector('.dropdown').addEventListener('mouseover', function() {
 document.querySelector('.dropdown').addEventListener('mouseout', function() {
     this.children[1].style.display = 'none';
 });
+
+
+/*Music Player*/ 
+
+
+let songs = [
+    {src: "/songs/Chris Anich - Dime si no.mp3", title: "Dime si no", artist: "Chris Anich"},
+    {src: "/songs/Chris Anich - Bienvenidos a la Tierra.mp3", title: "Bienvenidos a la Tierrra", artist: "Chris Anich"},
+    {src: "/songs/DSYS - Escalava de nadie.mp3", title: "Esclava de nadie", artist: "De Sombras y Siluetas"},
+    {src: "/songs/DSYS - Ladrandole a la luna.mp3", title: "Bienvenidos a la Tierrra", artist: "De Sombras y Siluetas"},
+];
+
+let currentSong = 0;
+let audio = new Audio(songs[currentSong].src);
+let isPlaying = false;
+
+function playPauseSong() {
+    if (isPlaying) {
+        audio.pause();
+    } else {
+        audio.play();
+    }
+    isPlaying = !isPlaying;
+}
+
+function nextSong() {
+    audio.pause();
+    currentSong++;
+    if (currentSong > songs.length - 1) {
+        currentSong = 0;
+    }
+    audio.src = songs[currentSong].src;
+    audio.load();
+    audio.play();
+}
+
+function prevSong() {
+    audio.pause();
+    currentSong--;
+    if (currentSong < 0) {
+        currentSong = songs.length - 1;
+    }
+    audio.src = songs[currentSong].src;
+    audio.load();
+    audio.play();
+}
+
+let playButton = document.getElementById('play_btn');
+if (playButton) {
+    playButton.addEventListener('click', playPauseSong);
+}
+
+let nextButton = document.getElementById('next_btn');
+if (nextButton) {
+    nextButton.addEventListener('click', nextSong);
+}
+
+let prevButton = document.getElementById('prev_btn');
+if (prevButton) {
+    prevButton.addEventListener('click', prevSong);
+}
+
+let playlistSongs = document.querySelectorAll('.p_song');
+
+playlistSongs.forEach((songElement, index) => {
+    songElement.querySelector('button').addEventListener('click', () => {
+        playSong(index);
+    });
+});
+
+function playSong(index) {
+    if (currentSong === index) {
+        // If the song that is being played is the same one that is already playing,
+        // toggle the isPlaying variable and either play or pause the song.
+        if (isPlaying) {
+            audio.pause();
+        } else {
+            audio.play();
+        }
+        isPlaying = !isPlaying;
+    } else {
+        // If the song that is being played is a different one, stop the current song,
+        // change the current song to the selected one, and start playing it.
+        audio.pause();
+        currentSong = index;
+        audio.src = songs[currentSong].src;
+        audio.load();
+        audio.play();
+        isPlaying = true;
+    }
+}
